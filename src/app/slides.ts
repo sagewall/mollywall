370c11faf6484d54d8972430a133c70c170c310a
Sage Wall <sagewall@users.noreply.github.com>
2020-06-12 15:41:43 -0600

    Add descriptive text from slides

--- src/app/slides.ts
+++ src/app/slides.ts
@@ -1,5 +1,22 @@
 export const SLIDES = [
-  { src: '/assets/IMG_20200111_122804.jpg' },
-  { src: '/assets/IMG_20200304_203450.jpg' },
-  { src: '/assets/IMG_20200420_124604.jpg' },
+  {
+    date: 'January 11, 2020',
+    description: 'Molly over at the park with her wheel chair.',
+    headline: 'Molly with her cart',
+    src: '/assets/IMG_20200111_122804.jpg',
+  },
+  {
+    date: 'March 04, 2020',
+    description:
+      'She would come up with some pretty crazy ways of laying down.',
+    headline: 'Just Hanging Out',
+    src: '/assets/IMG_20200304_203450.jpg',
+  },
+  {
+    date: 'April 20, 2020',
+    description:
+      'Towards the end when she could not walk Molly loved to go on wagon rides at Daniles park.',
+    headline: 'Molly in her wagon',
+    src: '/assets/IMG_20200420_124604.jpg',
+  },
 ];
