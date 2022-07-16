9addb28804fce837407af82364fea52a2631783c
Sage Wall <sagewall@users.noreply.github.com>
2022-07-16 10:56:53 -0600

    ✨ Add slide definitions

---
+++ src/slide.ts
@@ -0,0 +1,6 @@
+export interface Slide {
+  date: string;
+  description: string;
+  headline: string;
+  src: string;
+}
---
+++ src/slides.ts
@@ -0,0 +1,177 @@
+import type { Slide } from "./slide";
+
+export const SLIDES: Slide[] = [
+  {
+    date: "January 4, 2008",
+    description:
+      "This is the day I first saw Molly and instantly fell in love with her.  She was the only brindle female puppy in the litter and was so cute.",
+    headline: "Introductions",
+    src: "/assets/20080104.jpg"
+  },
+  {
+    date: "January 17, 2008",
+    description: `The first few nights I slept on the floor next to her crate so she wouldn't be so scared.`,
+    headline: "First day home",
+    src: "/assets/20080117.jpg"
+  },
+  {
+    date: "January 19, 2008",
+    description:
+      "Molly was so cute and loving.  This photo was taken just a couple days after coming home with me.",
+    headline: "What a cute puppy",
+    src: "/assets/20080119.jpg"
+  },
+  {
+    date: "April 4, 2008",
+    description: `Molly quickly outgrew her first bed but that didn't stop her from still trying to fit.`,
+    headline: "They grow up so fast",
+    src: "/assets/20080404.jpg"
+  },
+  {
+    date: "May 5, 2008",
+    description: "She loved to use her stuffed animals as pillows.",
+    headline: "Resting in the sun with my lion",
+    src: "/assets/20080502.jpg"
+  },
+  {
+    date: "June 5, 2008",
+    description: "Taking a nap on her bed.",
+    headline: "Nap time",
+    src: "/assets/20080605.jpg"
+  },
+  {
+    date: "November 23, 2008",
+    description:
+      'Playing with her first birthday stuffed animal.  It was forever known as "birthday cow".',
+    headline: "First Birthday",
+    src: "/assets/20081123.jpg"
+  },
+  {
+    date: "January 01, 2009",
+    description: "Can we please go to the dog park?",
+    headline: "I am so bored",
+    src: "/assets/20090124.jpg"
+  },
+  {
+    date: "October 31, 2009",
+    description:
+      "Molly loved Halloween and to greet all the little kids arriving at the door. She loved little kids.",
+    headline: "Pumpkin",
+    src: "/assets/20091031.jpg"
+  },
+  {
+    date: "May 26, 2010",
+    description: "She had such a big happy goofy smile.",
+    headline: "Big happy goofy smile",
+    src: "/assets/20100526.jpg"
+  },
+  {
+    date: "January 2, 2011",
+    description: "Life is tough.",
+    headline: "Life is tough",
+    src: "/assets/20110102.jpg"
+  },
+  {
+    date: "February 11, 2011",
+    description: "She loved to hang out at the Louisville dog park.",
+    headline: "Dog park",
+    src: "/assets/20110211.jpg"
+  },
+  {
+    date: "February 19, 2011",
+    description:
+      "When Molly would want something she would try and stare me into submission.  She wanted desperately to get into the pond at the dog park.",
+    headline: "Stare down",
+    src: "/assets/20110219.jpg"
+  },
+  {
+    date: "September 11, 2012",
+    description: "She loved her stuffed animals.  She treated them like her children.",
+    headline: "Stuffed animals",
+    src: "/assets/20120911.jpg"
+  },
+  {
+    date: "December 20, 2012",
+    description: "We got our picture taken with Santa Clause.",
+    headline: "Christmas Card",
+    src: "/assets/20121220.jpg"
+  },
+  {
+    date: "October 29, 2016",
+    description:
+      "Molly was diagnosed with cancer on July 4, 2016. After a few months of chemotherapy she lost some hair in the muzzle but lived for another four years.",
+    headline: "Chemotherapy hair loss",
+    src: "/assets/20161029.jpg"
+  },
+  {
+    date: "October 19, 2018",
+    description:
+      "Molly drooled a lot when she was hot and nearly every walk she would have a hanger.",
+    headline: "Dog drool",
+    src: "/assets/20181019.jpg"
+  },
+  {
+    date: "January 3, 2019",
+    description: "She sure loved to roll around in dry grass.",
+    headline: "Dry grass rolling",
+    src: "/assets/20190103.jpg"
+  },
+  {
+    date: "November 4, 2019",
+    description:
+      "Molly started having difficulty walking in late summer of 2019.  She loved to lay down in this same spot on our walks and rest.  She liked to just lay down and watch the what was going on in the park.",
+    headline: "Park supervisor",
+    src: "/assets/20191104.jpg"
+  },
+  {
+    date: "November 4, 2019",
+    description:
+      "She went to physical therapy twice a week.  She used an underwater treadmill and it really helped strengthen her back legs.",
+    headline: "Underwater treadmill",
+    src: "/assets/20191203.jpg"
+  },
+  {
+    date: "January 11, 2020",
+    description: `When she couldn't support herself with her back legs we borrowed a wheel chair from the physical therapist.`,
+    headline: "Molly with her wheel chair",
+    src: "/assets/20200111.jpg"
+  },
+  {
+    date: "March 04, 2020",
+    description:
+      "Sometimes she seemed to pose for the camera.  She would come up with some pretty crazy ways of laying down.",
+    headline: "Just hanging out",
+    src: "/assets/20200304.jpg"
+  },
+  {
+    date: "April 12, 2020",
+    description: `She was always playful and happy.  Even when she couldn't run around she still found toys to play with.`,
+    headline: "Playing with my toys",
+    src: "/assets/20200412.jpg"
+  },
+  {
+    date: "April 20, 2020",
+    description: `Towards the end when she could not walk she loved to go on wagon rides at the park.`,
+    headline: "Molly in her wagon",
+    src: "/assets/20200420.jpg"
+  },
+  {
+    date: "May 26, 2020",
+    description:
+      "The day before she passed she just seemed to get really tired and sleepy.  She never seemed to be in any pain.  It was just time to go.",
+    headline: "Falling asleep",
+    src: "/assets/20200526.jpg"
+  },
+  {
+    date: "June 5, 2020",
+    description: "My heart will always bear the paw prints left by you.",
+    headline: "Paw print",
+    src: "/assets/20200605.jpg"
+  },
+  {
+    date: "June 5, 2020",
+    description: "I love you my dear sweet Molly and always will.",
+    headline: "Love",
+    src: "/assets/20200615.jpg"
+  }
+];
