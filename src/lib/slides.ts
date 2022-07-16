58821531e418f32bff9d3fe812c0583de522935e
Sage Wall <sagewall@users.noreply.github.com>
2022-07-16 17:53:16 -0600

    🍱 Move assets into lib folder

--- src/lib/slides.ts
+++ src/lib/slides.ts
@@ -1,3 +1,30 @@
+import img20080104 from "$lib/assets/20080104.jpg";
+import img20080117 from "$lib/assets/20080117.jpg";
+import img20080119 from "$lib/assets/20080119.jpg";
+import img20080404 from "$lib/assets/20080404.jpg";
+import img20080502 from "$lib/assets/20080502.jpg";
+import img20080605 from "$lib/assets/20080605.jpg";
+import img20081123 from "$lib/assets/20081123.jpg";
+import img20090124 from "$lib/assets/20090124.jpg";
+import img20091031 from "$lib/assets/20091031.jpg";
+import img20100526 from "$lib/assets/20100526.jpg";
+import img20110102 from "$lib/assets/20110102.jpg";
+import img20110211 from "$lib/assets/20110211.jpg";
+import img20110219 from "$lib/assets/20110219.jpg";
+import img20120911 from "$lib/assets/20120911.jpg";
+import img20121220 from "$lib/assets/20121220.jpg";
+import img20161029 from "$lib/assets/20161029.jpg";
+import img20181019 from "$lib/assets/20181019.jpg";
+import img20190103 from "$lib/assets/20190103.jpg";
+import img20191104 from "$lib/assets/20191104.jpg";
+import img20191203 from "$lib/assets/20191203.jpg";
+import img20200111 from "$lib/assets/20200111.jpg";
+import img20200304 from "$lib/assets/20200304.jpg";
+import img20200412 from "$lib/assets/20200412.jpg";
+import img20200420 from "$lib/assets/20200420.jpg";
+import img20200526 from "$lib/assets/20200526.jpg";
+import img20200605 from "$lib/assets/20200605.jpg";
+import img20200615 from "$lib/assets/20200615.jpg";
 import type { Slide } from "./slide";
 
 export const SLIDES: Slide[] = [
@@ -6,199 +33,199 @@ export const SLIDES: Slide[] = [
     description:
       "This is the day I first saw Molly and instantly fell in love with her.  She was the only brindle female puppy in the litter and was so cute.",
     headline: "Introductions",
-    id: "slide-1",
-    src: "/assets/20080104.jpg"
+    id: "img20080104",
+    src: img20080104
   },
   {
     date: "January 17, 2008",
     description: `The first few nights I slept on the floor next to her crate so she wouldn't be so scared.`,
     headline: "First day home",
-    id: "slide-2",
-    src: "/assets/20080117.jpg"
+    id: "img20080117",
+    src: img20080117
   },
   {
     date: "January 19, 2008",
     description:
       "Molly was so cute and loving.  This photo was taken just a couple days after coming home with me.",
     headline: "What a cute puppy",
-    id: "slide-3",
-    src: "/assets/20080119.jpg"
+    id: "img20080119",
+    src: img20080119
   },
   {
     date: "April 4, 2008",
     description: `Molly quickly outgrew her first bed but that didn't stop her from still trying to fit.`,
     headline: "They grow up so fast",
-    id: "slide-4",
-    src: "/assets/20080404.jpg"
+    id: "img20080404",
+    src: img20080404
   },
   {
     date: "May 5, 2008",
     description: "She loved to use her stuffed animals as pillows.",
     headline: "Resting in the sun with my lion",
-    id: "slide-5",
-    src: "/assets/20080502.jpg"
+    id: "img20080502",
+    src: img20080502
   },
   {
     date: "June 5, 2008",
     description: "Taking a nap on her bed.",
     headline: "Nap time",
-    id: "slide-6",
-    src: "/assets/20080605.jpg"
+    id: "img20080605",
+    src: img20080605
   },
   {
     date: "November 23, 2008",
     description:
       'Playing with her first birthday stuffed animal.  It was forever known as "birthday cow".',
     headline: "First Birthday",
-    id: "slide-7",
-    src: "/assets/20081123.jpg"
+    id: "img20081123",
+    src: img20081123
   },
   {
     date: "January 01, 2009",
     description: "Can we please go to the dog park?",
     headline: "I am so bored",
-    id: "slide-8",
-    src: "/assets/20090124.jpg"
+    id: "img20090124",
+    src: img20090124
   },
   {
     date: "October 31, 2009",
     description:
       "Molly loved Halloween and to greet all the little kids arriving at the door. She loved little kids.",
     headline: "Pumpkin",
-    id: "slide-9",
-    src: "/assets/20091031.jpg"
+    id: "img20091031",
+    src: img20091031
   },
   {
     date: "May 26, 2010",
     description: "She had such a big happy goofy smile.",
     headline: "Big happy goofy smile",
-    id: "slide-10",
-    src: "/assets/20100526.jpg"
+    id: "img20100526",
+    src: img20100526
   },
   {
     date: "January 2, 2011",
     description: "Life is tough.",
     headline: "Life is tough",
-    id: "slide-11",
-    src: "/assets/20110102.jpg"
+    id: "img20110102",
+    src: img20110102
   },
   {
     date: "February 11, 2011",
     description: "She loved to hang out at the Louisville dog park.",
     headline: "Dog park",
-    id: "slide-12",
-    src: "/assets/20110211.jpg"
+    id: "img20110211",
+    src: img20110211
   },
   {
     date: "February 19, 2011",
     description:
       "When Molly would want something she would try and stare me into submission.  She wanted desperately to get into the pond at the dog park.",
     headline: "Stare down",
-    id: "slide-13",
-    src: "/assets/20110219.jpg"
+    id: "img20110219",
+    src: img20110219
   },
   {
     date: "September 11, 2012",
     description: "She loved her stuffed animals.  She treated them like her children.",
     headline: "Stuffed animals",
-    id: "slide-14",
-    src: "/assets/20120911.jpg"
+    id: "img20120911",
+    src: img20120911
   },
   {
     date: "December 20, 2012",
     description: "We got our picture taken with Santa Clause.",
     headline: "Christmas Card",
-    id: "slide-15",
-    src: "/assets/20121220.jpg"
+    id: "img20121220",
+    src: img20121220
   },
   {
     date: "October 29, 2016",
     description:
       "Molly was diagnosed with cancer on July 4, 2016. After a few months of chemotherapy she lost some hair in the muzzle but lived for another four years.",
     headline: "Chemotherapy hair loss",
-    id: "slide-16",
-    src: "/assets/20161029.jpg"
+    id: "img20161029",
+    src: img20161029
   },
   {
     date: "October 19, 2018",
     description:
       "Molly drooled a lot when she was hot and nearly every walk she would have a hanger.",
     headline: "Dog drool",
-    id: "slide-17",
-    src: "/assets/20181019.jpg"
+    id: "img20181019",
+    src: img20181019
   },
   {
     date: "January 3, 2019",
     description: "She sure loved to roll around in dry grass.",
     headline: "Dry grass rolling",
-    id: "slide-18",
-    src: "/assets/20190103.jpg"
+    id: "img20190103",
+    src: img20190103
   },
   {
     date: "November 4, 2019",
     description:
       "Molly started having difficulty walking in late summer of 2019.  She loved to lay down in this same spot on our walks and rest.  She liked to just lay down and watch the what was going on in the park.",
     headline: "Park supervisor",
-    id: "slide-19",
-    src: "/assets/20191104.jpg"
+    id: "img20191104",
+    src: img20191104
   },
   {
     date: "November 4, 2019",
     description:
       "She went to physical therapy twice a week.  She used an underwater treadmill and it really helped strengthen her back legs.",
     headline: "Underwater treadmill",
-    id: "slide-20",
-    src: "/assets/20191203.jpg"
+    id: "img20191203",
+    src: img20191203
   },
   {
     date: "January 11, 2020",
     description: `When she couldn't support herself with her back legs we borrowed a wheel chair from the physical therapist.`,
     headline: "Molly with her wheel chair",
-    id: "slide-21",
-    src: "/assets/20200111.jpg"
+    id: "img20200111",
+    src: img20200111
   },
   {
     date: "March 04, 2020",
     description:
       "Sometimes she seemed to pose for the camera.  She would come up with some pretty crazy ways of laying down.",
     headline: "Just hanging out",
-    id: "slide-22",
-    src: "/assets/20200304.jpg"
+    id: "img20200304",
+    src: img20200304
   },
   {
     date: "April 12, 2020",
     description: `She was always playful and happy.  Even when she couldn't run around she still found toys to play with.`,
     headline: "Playing with my toys",
-    id: "slide-23",
-    src: "/assets/20200412.jpg"
+    id: "img20200412",
+    src: img20200412
   },
   {
     date: "April 20, 2020",
     description: `Towards the end when she could not walk she loved to go on wagon rides at the park.`,
     headline: "Molly in her wagon",
-    id: "slide-24",
-    src: "/assets/20200420.jpg"
+    id: "img20200420",
+    src: img20200420
   },
   {
     date: "May 26, 2020",
     description:
       "The day before she passed she just seemed to get really tired and sleepy.  She never seemed to be in any pain.  It was just time to go.",
     headline: "Falling asleep",
-    id: "slide-25",
-    src: "/assets/20200526.jpg"
+    id: "img20200526",
+    src: img20200526
   },
   {
     date: "June 5, 2020",
     description: "My heart will always bear the paw prints left by you.",
     headline: "Paw print",
-    id: "slide-26",
-    src: "/assets/20200605.jpg"
+    id: "img20200605",
+    src: img20200605
   },
   {
     date: "June 5, 2020",
     description: "I love you my dear sweet Molly and always will.",
     headline: "Love",
-    id: "slide-27",
-    src: "/assets/20200615.jpg"
+    id: "img20200615",
+    src: img20200615
   }
 ];
