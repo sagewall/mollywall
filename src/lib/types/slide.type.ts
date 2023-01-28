b9faae2d3ba7a46f873a082d1e98e7ee61facc9e
Sage Wall <sagewall@users.noreply.github.com>
2023-01-28 07:46:36 -0700

    code formatting

--- src/lib/types/slide.type.ts
+++ src/lib/types/slide.type.ts
@@ -1,7 +1,7 @@
 export type Slide = {
-	date: string;
-	description: string;
-	headline: string;
-	id: string;
-	src: string;
+    date: string;
+    description: string;
+    headline: string;
+    id: string;
+    src: string;
 };
